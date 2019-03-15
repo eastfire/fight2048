873df7735429be1b1af429a4ee8df3ad923cb037
heyc <yche@mo9.com>
2019-03-15 11:29:39 +0800

    fix

--- assets/Script/global.js
+++ assets/Script/global.js
@@ -58,7 +58,7 @@ export default {
   heroInitSkill: [],
   // heroInitSkill: ["meteorShowerSkill"],
   basicSkill: ["coolingSkill","constitutionSkill","dexteritySkill","luckSkill"],
-  basicSkill: ["fireballSkill"],
+  // basicSkill: ["fireballSkill"],
   heroBasicSkill: {
     normal: ["forwardSlashSkill","horizontalSlashSkill","verticalSlashSkill"],
     wizard: ["missileSkill","spiderWebSkill","iceWallSkill"],
